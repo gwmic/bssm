e1377b1f12c8db3575e751e7b9d5fca5dac157a8
chocodao <grahammckee@protonmail.com>
2023-11-24 17:13:52 -0600

    add tunable confidence

--- bssm.py
+++ bssm.py
@@ -5,10 +5,10 @@ import threading
 import videobuffer as vb
 import wrappers
 
-SOURCE = 1 # Source num of webcam
+SOURCE = 0 # Source num of webcam
 SHOWPROCESS = True # Toggle the processing annotations; turning off saves 30% time vs. on
-CONFIDENCELIVE = 0.1 # Confidence for infernce on live vid feed to detect shot; value ranges from 0 to 1.0
-CONFIDENCEPROCESS = 0.1 # Confidence for infernce on cropped shots to be processed into data; value ranges from 0 to 1.0
+CONFIDENCELIVE = 0.2 # Confidence for infernce on live vid feed to detect shot; value ranges from 0 to 1.0
+CONFIDENCEPROCESS = 0.45 # Confidence for infernce on cropped shots to be processed into data; value ranges from 0 to 1.0
 
 # manages all global data as data object
 class DataMan:
@@ -26,7 +26,7 @@ class DataMan:
 
     def init_arrays(self):
         # Initialize all array attributes to empty numpy arrays
-        self.ballArr = self.shotArr = self.laneArr = self.laneDimArr = self.bssmDimArr = self.poly = self.croppedPoly = self.croppedLaneArr = np.array([])
+        self.ballArr = self.shotArr = self.laneArr = self.laneDimArr = self.bssmDimArr = self.poly = self.croppedPoly = self.croppedLaneArr = self.curveArr = np.array([])
 
     def init_flags(self):
         # Initialize all boolean flags
@@ -48,7 +48,7 @@ customRender = wrappers.renderWrapper(data)
 inference.Stream(
     source=data.source,
     model="bowling-model/6",
-    confidence=0.1,
+    confidence=data.confidenceLive,
     iou_threshold=0.01,
     output_channel_order="BGR",
     use_main_thread=True,
